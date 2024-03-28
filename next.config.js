ea50defeef73a18c5478c50f0890ed0fe4f61808
RVYA <y.muratcaylak@outlook.com>
2024-03-29 00:46:44 +0300

    Rename the repository to "ram-character-search". Configure the project to use with GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath: "/ram-character-search",
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
